e7421730ee8d5d6ec7982d28eed67f2a7c8cc61c
Dithesh B <ditheshgowda692@gmail.com>
2025-08-24 04:46:35 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "accessguard-hnzrd",
-  "appId": "1:241317652095:web:60cf3637ae2b50db85414a",
-  "storageBucket": "accessguard-hnzrd.firebasestorage.app",
-  "apiKey": "AIzaSyAxRxvCDQYq3fhSf10FTTfumSAvnEmzKKM",
-  "authDomain": "accessguard-hnzrd.firebaseapp.com",
-  "messagingSenderId": "241317652095"
+  apiKey: "AIzaSyCrTk09il9LyR0iIyQ_PMbQ62xC8tqJ0Xs",
+  authDomain: "accessguard-hnzrd.firebaseapp.com",
+  projectId: "accessguard-hnzrd",
+  storageBucket: "accessguard-hnzrd.firebasestorage.app",
+  messagingSenderId: "241317652095",
+  appId: "1:241317652095:web:60cf3637ae2b50db85414a"
 };
 
 // Initialize Firebase
